99ce33b2f902ceaa147aea38059e86ff4271cb3c
davydovanataly91 <davydovanataly@gmail.com>
2025-06-22 13:19:29 +0300

    Scripts

---
+++ script1.py
@@ -0,0 +1,2 @@
+employee_list = ["John Snow", "Piter Pen", "Drakula", "IvanIV", "Moana"]
+print(employee_list[1] + "," + employee_list[-2])
---
+++ script3.py
@@ -0,0 +1,11 @@
+import math
+
+
+def min_boxes(items):
+    return math.ceil(items/5)
+
+
+num_items = int(input("Введите количество предметов: "))
+
+
+print(f"Минимальное количество коробок: {min_boxes(num_items)}")
